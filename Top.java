6860dfc1d020bf434f4840dd3f14109996648c0c
kolya19951 <kolya.simotyuk@gmail.com>
2015-03-21 21:25:56 +0300

    Create Top.java

---
+++ Top.java
@@ -0,0 +1,20 @@
+package com;
+
+public class Top {
+    public Vector_4d position;
+    public int number;
+    public Vector_4d position4d;
+    public Vector_4d position3d;
+    public Vector_4d position2d;
+    Top(){
+        this.position = new Vector_4d();
+        this.number = 0;
+        this.position4d = new Vector_4d();
+        this.position3d = new Vector_4d();
+        this.position2d = new Vector_4d();
+    }
+    Top(Vector_4d a, int numer){
+        this.position = a;
+        this.number = number;
+    }
+}
